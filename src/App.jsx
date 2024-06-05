2c7f203d0767d681b4166fbdad146ec293b9077a
luizaRodrigues <luiza.rodrigues.cp@gmail.com>
2024-06-05 14:15:51 -0300

    pequenas mudanças nos arquivos base (limpeza e organização)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import Header from './Components/Header'
 import Footer from './Components/Footer'
-import Home from './Routers/Home'
-
 import { Outlet } from 'react-router-dom'
 import './App.css'
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Home from './Routers/Home'
-import Portfolio from './Routers/Portfolio'
-import Servicos from './Routers/Servicos'
 import Sobre from './Routers/Sobre'
+import Servicos from './Routers/Servicos'
+import Portfolio from './Routers/Portfolio'
 import GaleriaFotos from './Routers/GaleriaFotos'
 import ErrorPage from './Routers/ErrorPage'
 
