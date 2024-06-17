38e43feb3d7e7f8b9d648fa454fab1f8aa6ad799
luizaRodrigues <luiza.rodrigues.cp@gmail.com>
2024-06-17 16:37:12 -0300

    adição de configurações para diminuir tamanho de foto

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      /* pass your config */
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
   base: './'
 })
